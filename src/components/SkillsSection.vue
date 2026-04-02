9de957f286a64f399fca43b80c6c5233a0a27805
IsnantoBudi <isnantobudi0@gmail.com>
2026-04-02 10:47:52 +0700

    add SEO

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -9,7 +9,7 @@ const barsVisible = ref(false);
 onMounted(() => {
   const observer = new IntersectionObserver(
     (entries) => {
-      if (entries[0].isIntersecting) {
+      if (entries[0]?.isIntersecting) {
         barsVisible.value = true;
         observer.disconnect();
       }
